54107db13edc2e4fd5b79da4de1700ff55880e22
mkao006 <mkao006@gmail.com>
2014-06-26 17:36:03 +0200

    Added roxygen declaration

--- faoswsFlag/codes/flag2weight.R
+++ faoswsFlag/codes/flag2weight.R
@@ -1,3 +1,14 @@
+##' Function to convert flag to information weight.
+##'
+##' The flag is converted to a numeric information weight for
+##' quantification and further manipulation.
+##'
+##' @param flagObservationStatus The flag
+##' @param flagTable The table mapping the flag and weights
+##'
+##' @export
+##' 
+
 flag2weight = function(flagObservationStatus, flagTable){
     index = match(flagObservationStatus, flagTable$flagObservationStatus)
     as.numeric(flagTable$flagObservationWeights[index])
