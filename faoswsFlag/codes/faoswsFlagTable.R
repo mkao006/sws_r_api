781e765032a1153df72d1bb2171cb6b8a5d16e2e
mkao006 <mkao006@gmail.com>
2014-07-10 17:04:51 +0200

    full commit of the package

---
+++ faoswsFlag/codes/faoswsFlagTable.R
@@ -0,0 +1,10 @@
+##' Flag table for the new FAO statistical working system
+##'
+##' The table maps the relationship between the observational status
+##' flag and it's corresponding information weight
+##'
+##' @docType data
+##' @keywords datasets
+##' @name faoswsFlagTable
+##'
+NULL
---
+++ faoswsFlag/faoswsFlag/R/aggregateObservationFlag.R
@@ -0,0 +1,34 @@
+##' This function performs aggregation on the flag
+##'
+##' The aggregation is based on the weight of the flag as designated
+##' by the table. Official sources has high weight while data from
+##' unreliable sources has low weight.
+##'
+##' @param ... Vectors of Flags, see detail
+##' @param flagTable The table which maps the relationship between the
+##' flag and its information weight.
+##'
+##' @details The arguement for flag is the same to the function
+##' sum. Multiple instances of the flag can be pass in to the function
+##' to be aggregated.
+##'
+##' @seealso \code{\link{sum}}
+##'
+##' @export
+##' 
+
+aggregateObservationFlag = function(..., flagTable = faoswsFlagTable){
+    
+    flags = list(...)
+    
+    ## Convert flags to numeric information weight as determined by
+    ## the table.
+    weights = lapply(X = flags, FUN = flag2weight, flagTable = flagTable)
+
+    ## Aggregate the weight by taking the lowest weight
+    aggregatedWeights = do.call(pmin, weights)
+
+    ## Convert weight back to flag
+    aggregatedFlag = weight2flag(aggregatedWeights, flagTable)
+    aggregatedFlag
+}
