74ee50e8357f50ad3490f0005d1567bb2323de91
mkao006 <mkao006@gmail.com>
2014-08-07 23:36:52 +0200

    restructured code to three seperate function and one execution function, added formula and prefix tuples

--- r_modules/compute_yield/computeYieldModule.R
+++ r_modules/compute_yield/computeYieldModule.R
@@ -4,58 +4,115 @@
 ## Author: Michael. C. J. Kao
 ########################################################################
 
-## NOTE (Michael): Need to find a way of handling formulas.
-
-## load the library
-library(faosws)
-library(faoswsFlag)
-library(faoswsExtra)
 
 ## Set up for the test environment
 if(Sys.getenv("USER") == "mk"){
     GetTestEnvironment(
         baseUrl = "https://hqlqasws1.hq.un.fao.org:8181/sws",
-        token = "2a4d62c3-e776-4854-a013-0a4cdb5d7541"
+        token = "4ad7bc88-5521-4be1-81e2-a1a5b3b6413f"
         )
 }
 
 
-## Pivot to vectorize yield computation
-newPivot = c(
-    Pivoting(code= "geographicAreaM49", ascending = TRUE),
-    Pivoting(code= "measuredItemCPC", ascending = TRUE),
-    Pivoting(code = "timePointYears", ascending = FALSE),
-    Pivoting(code= "measuredElement", ascending = TRUE)
-    )
+## Function for obtaining the data and meta data.
+getYieldData = function(dataContext){
+    ## Setting the tuples, this should be accessed by the API
+    formulaTuples =
+        data.table(
+            output = "5510",
+            input = "5312",
+            productivity = "5421"
+            )
+    
+    ## setting the prefix, also should be accessed by the API
+    prefixTuples =
+        data.table(
+            valuePrefix = "Value_measuredElement_",
+            flagObsPrefix = "flagObservationStatus_measuredElement_",
+            flagMethodPrefix = "flagMethod_measuredElement_"
+            )
 
-## Query the data
-query = GetData(
-    key = swsContext.datasets[[1]],
-    flags = TRUE,
-    normalized = FALSE,
-    pivoting = newPivot
-)
+    ## Pivot to vectorize yield computation
+    newPivot = c(
+        Pivoting(code= "geographicAreaM49", ascending = TRUE),
+        Pivoting(code= "measuredItemCPC", ascending = TRUE),
+        Pivoting(code = "timePointYears", ascending = FALSE),
+        Pivoting(code= "measuredElement", ascending = TRUE)
+        )
+    
+    ## Query the data
+    query = GetData(
+        key = dataContext,
+        flags = TRUE,
+        normalized = FALSE,
+        pivoting = newPivot
+        )
+    
+    list(query = query,
+         formulaTuples = formulaTuples,
+         prefixTuples = prefixTuples)
+}
 
 
-## Compute the yield
-query[, Value_measuredElement_5421 :=
-      computeRatio(Value_measuredElement_5510,
-                   Value_measuredElement_5312)]
+## Function to compute the yield data
+computeYieldData = function(data, formulaTuples, prefixTuples){
+    for(i in NROW(formulaTuples)){
+        ## set the names
+        valueNames =
+            as.list(paste0(prefixTuples$valuePrefix, formulaTuples[i]))
+        names(valueNames) =
+            colnames(formulaTuples)
+        flagObsNames =
+            as.list(paste0(prefixTuples$flagObsPrefix,formulaTuples[i]))
+        names(flagObsNames) = colnames(formulaTuples)
+        ## Compute Value
+        data[, eval(valueNames$productivity) :=
+             computeRatio(get(valueNames$output),
+                          get(valueNames$input))]             
+        ## Compute observation flag
+        data[, eval(flagObsNames$productivity) :=
+             aggregateObservationFlag(get(flagObsNames$output),
+                                      get(flagObsNames$input))]
+        ## Assign method flag
+        data[, eval(paste0(prefixTuples$flagMethod,
+                           formulaTuples$productivity[i])) := "i"]
+    }
+}
 
-## Compute observation flags for yield
-query[, flagObservationStatus_measuredElement_5421 :=
-      aggregateObservationFlag(flagObservationStatus_measuredElement_5510,
-                               flagObservationStatus_measuredElement_5312,
-                               faoswsFlagTable)]
+## computeYieldData(data = query, formulaTuples = formulaTuples,
+##                  prefixTuples = prefixTuples)
 
-## Append the method flags for yield
-query[, flagMethod_measuredElement_5421 :=
-      as.character(ifelse(is.na(Value_measuredElement_5421), NA, "i"))]
 
-## Add the newly computed key back to the dimension
-addKey(swsContext.datasets[[1]]@dimensions[["measuredElement"]]) = "5421"
+## Function to save data back
+saveYieldData = function(dataContext, data){
+    SaveData(domain = slot(dataContext[[1]], "domain"),
+             dataset = slot(dataContext[[1]], "dataset"),
+             data = data, normalized = FALSE)
+}
 
-## write back to the data
-SaveData(domain = slot(swsContext.datasets[[1]], "domain"),
-         dataset = slot(swsContext.datasets[[1]], "dataset"),
-         data = query, normalized = FALSE)
+
+## Function to execute the whole yield module
+executeYieldModule = function(){
+    require(faoswsFlag)
+    require(faoswsExtra)
+    compute = try(
+        datasets = getYieldData(swsContext.datasets[[1]])
+        with(datasets,
+             {
+                 computeYieldData(data = query,
+                                  formulaTuples = formulaTuples,
+                                  prefixTuples = prefixTuples)
+                 saveYieldData(dataContext = swsContext.datasets[[1]],
+                               data = query)
+             }
+             )
+        )
+    if(inherits(compute, "try-error")){
+        print("Yield Module Failed")
+    } else {
+        print("Yield Module Executed Successfully")
+    }
+}
+
+executeYieldModule()
+
