2ef82129b80166a0924be02773930e1fbfe1b8b6
mkao006 <mkao006@gmail.com>
2015-02-27 17:16:56 +0100

    actually implemented the module

--- r_modules/fbs_balancing/fbsBalancing.R
+++ r_modules/fbs_balancing/fbsBalancing.R
@@ -6,6 +6,11 @@ suppressMessages({
     library(conSTable)
 })
 
+## NOTE (Michael): The selected country code is in old FAO
+##                 classification, need to change to M49 codes when
+##                 the contingency table is set up.
+selectedCountry = "231"
+selectedYear = "2010"
 
 ## Set up testing environments
 if(Sys.getenv("USER") == "mk"){
@@ -16,3 +21,37 @@ if(Sys.getenv("USER") == "mk"){
 }
 
 
+## Function to get the compiled contingency table
+getContingencyTable = function(){
+    ## TODO (Michael): Need to wait for the compiler of other elements
+    ##                 to finish and also get Nick to set up the
+    ##                 table.
+    readFBS(file = "adjustedCommodityContigencyTable.csv",
+            file0 = "structuralZeroParameter.csv",
+            filef = "adjustedFeedRange.csv")
+}
+
+## Function to selecte the best table from the sampling
+sampleBestTable = function(contingencyTable, sanityCheck = FALSE, maxErr = 10,
+    selectedCountry, selectedYear, ...){
+    
+    balancingFunction =
+        balanceFBS(FBS = contingencyTable,
+                   sanityCheck = sanityCheck, maxErr = maxErr)
+    sampledTables =
+        balancingFunction(Country = selectedCountry, year = selectedYear, ...)
+    sampledTables@bestTab
+}
+
+## Carry out the balancing
+bestBalancedTable =
+    getContingencyTable() %>%
+    sampleBestTable(contingencyTable = .,
+                    selectedCountry = selectedCountry,
+                    selectedYear = selectedYear,
+                    oset = c(30, 30, 30, 30, 30, 10000),
+                    prop = NULL,
+                    nIter = 1000,
+                    verbose = TRUE,
+                    checks = "none",
+                    feedShift = 30)
