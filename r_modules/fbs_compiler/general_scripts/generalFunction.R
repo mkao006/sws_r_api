d3642223878987edc9c6b9bab40e9dc31a2be00c
mkao006 <mkao006@gmail.com>
2015-03-07 17:30:46 +0100

    added merge all dataset function

--- r_modules/fbs_compiler/general_scripts/generalFunction.R
+++ r_modules/fbs_compiler/general_scripts/generalFunction.R
@@ -68,3 +68,15 @@ calorieStandardization = function(data, commodityTree, standardizeVariable,
              new = standardizeVariable)
     standardized
 }
+
+mergeAllData = function(...){
+    datasets = list(...)
+    Reduce(f = function(x, y){
+        keys = intersect(colnames(x), colnames(y))
+        setkeyv(x, keys)
+        setkeyv(y, keys)
+        merge(x, y, all = TRUE)
+    },
+           x = datasets[-1], init = datasets[[1]])
+}
+
