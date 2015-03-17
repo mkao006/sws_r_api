2b41184a218bc6918d0d2d2d0bd7e0d31fd7153f
mkao006 <mkao006@gmail.com>
2015-03-17 20:07:48 +0100

    added new elements and validation formula, also fixed some minor bugs

--- r_modules/production_validation/productionValidation.R
+++ r_modules/production_validation/productionValidation.R
@@ -7,7 +7,7 @@ suppressMessages({
     library(caret)
 })
 
-updateModel = FALSE
+updateModel = TRUE
 
 
 ## Year should be a paramameter selected.
@@ -30,7 +30,7 @@ reverseTradePrefix = "reverse_"
 if(Sys.getenv("USER") == "mk"){
     GetTestEnvironment(
         baseUrl = "https://hqlprswsas1.hq.un.fao.org:8181/sws",
-        token = "5c0de0e5-0705-4ccf-a798-f7a2f2e8adc2"
+        token = "6b16fcc4-8eb6-4cec-9009-680f11d0330a"
         )
     R_SWS_SHARE_PATH = getwd()
 } else {
@@ -69,11 +69,12 @@ getAllHistory = function(){
             Dimension(name = "geographicAreaM49",
                       keys = allCountries),
             Dimension(name = "measuredElement",
-                      keys = "5510"),
+                      keys = c("5312", "5510", "5412")),
             Dimension(name = "measuredItemCPC",
                       keys = allItems),
             Dimension(name = yearVar,
                       keys = as.character(allYears))
+                      ## keys = c("2009", "2010"))
             )
         )
 
@@ -86,18 +87,26 @@ getAllHistory = function(){
         )
 
     allHistory = GetHistory(key = newKey)
+
+    ## Convert time to numeric and levels to factor
     allHistory[, timePointYears := as.numeric(timePointYears)]
-    allHistory[, `:=`(c("geographicAreaM49", "measuredItemCPC",
+    allHistory[, `:=`(c("geographicAreaM49", "measuredItemCPC", "measuredElement",
                         "timePointYears", "flagMethod", "flagObservationStatus"),
                       list(factor(geographicAreaM49, levels = allCountries), 
                            factor(measuredItemCPC, level = allItems),
+                           factor(measuredElement,
+                                  levels = c("5312", "5510", "5412")),
                            as.numeric(timePointYears),
                            factor(flagMethod),
                            factor(flagObservationStatus)))]
+
+    ## Remove missing observation
     allHistory = allHistory[flagObservationStatus != "M", ]
-    ## There are no history before 1990
-    allHistory = allHistory[timePointYears >= 1900, ]
+
+    ## create the binary response
     allHistory[, valid := factor(ifelse(is.na(EndDate), "1", "0"))]
+
+    ## Removing duplicated values.
     allHistory[valid == "1", validValue := Value]
     allHistory[, validValue := na.omit(unique(.SD[, validValue])),
                by = c("geographicAreaM49", "measuredItemCPC", "measuredElement",
@@ -109,25 +118,96 @@ getAllHistory = function(){
 
 
 allHistory = getAllHistory()
-allHistory = allHistory[sample(NROW(allHistory), 300), ]
+
+validationRule =
+    valid ~ geographicAreaM49 + measuredItemCPC + measuredElement +
+        timePointYears + flagObservationStatus + flagMethod + Value
+
+## validationRule =
+##     valid ~ geographicAreaM49 + measuredItemCPC + 
+##         timePointYears + flagObservationStatus + flagMethod 
 
 
-## inTrain = createDataPartition(y = allHistory$valid, p = 0.75, list = FALSE)
-## training = allHistory[inTrain[, 1], ]
-## testing = allHistory[-inTrain[, 1], ]
+## Model paths
+## ---------------------------------------------------------------------
+logisticModelPath = paste0(R_SWS_SHARE_PATH, "/boostedLogisticValidationModel")
+lssvmModelPath = paste0(R_SWS_SHARE_PATH, "/lssvmValidationModel")
+rfModelPath = paste0(R_SWS_SHARE_PATH, "/rfValidationModel")
+fdaModelPath = paste0(R_SWS_SHARE_PATH, "/fdaValidationModel")
+bagEarthModelPath = paste0(R_SWS_SHARE_PATH, "/bagEarthValidationModel")
+
+
+## Control parameters
+## ---------------------------------------------------------------------
+logisticControl = trainControl(classProbs = TRUE,
+    summaryFunction = twoClassSummary)
+svmControl = trainControl(method = "cv", number = 5)
+rfControl = trainControl(method = "cv", number = 5)
+fdaControl = trainControl(method = "cv", number = 5)
+bagEarthControl = trainControl(method = "cv", number = 3)
+
+
+
+## ## This is for testing
+## ## ---------------------------------------------------------------------
+## subHistory = allHistory[sample(NROW(allHistory), 1250), ]
+
+
+## inTrain = createDataPartition(y = subHistory$valid, p = 0.75, list = FALSE)
+## training = subHistory[inTrain[, 1], ]
+## testing = subHistory[-inTrain[, 1], ]
+
+## logitBoostFit =
+##     train(validationRule,
+##           data = data.frame(training), method = "LogitBoost", metric = "ROC",
+##           trControl = logisticControl)
+## logitBoostPredicted = predict(logitBoostFit, testing)
+## confusionMatrix(data = logitBoostPredicted, testing$valid)
+
+## lssvmRadialFit =
+##     lssvm(validationRule,
+##           data = data.frame(training), kernel = "rbfdot")
+## lssvmRadialPredicted = predict(lssvmRadialFit, testing)
+## confusionMatrix(data = lssvmRadialPredicted, testing$valid)
+
+
+## rfFit =
+##     train(validationRule,
+##           data = data.frame(training), 
+##           method = "rf", prox = TRUE, allowParallel = TRUE,
+##           trControl = rfControl)
+## rfPredicted = predict(rfFit, testing)
+## confusionMatrix(data = rfPredicted, testing$valid)
+
+
+## fdaFit =
+##     train(validationRule, 
+##           data = data.frame(training),
+##           method = "fda",  trControl = fdaControl)
+## fdaPredicted = predict(fdaFit, testing)
+## confusionMatrix(data = fdaPredicted, testing$valid)
+
+
+bagEarthFit =
+    train(validationRule,
+          data = data.frame(training),
+          method = "bagEarth",  trControl = bagEarthControl,
+          tuneGrid = data.frame(degree = 1, nprune = 20))
+bagEarthPredicted = predict(bagEarthFit, testing)
+confusionMatrix(data = bagEarthPredicted, testing$valid)
+
+
+## The actual validation estimation starts from here
+## ---------------------------------------------------------------------
 
 
 ## Use the default control and train boosted classification tree
 
-logisticControl = trainControl(classProbs = TRUE,
-    summaryFunction = twoClassSummary)
-
 ## Model (1): Boosted logistic regression
-logisticModelPath = paste0(R_SWS_SHARE_PATH, "/boostedLogisticValidationModel")
+
 if(updateModel){
     logitBoostFit =
-        train(valid ~ geographicAreaM49 + measuredItemCPC + timePointYears +
-                  flagObservationStatus + flagMethod,
+        train(validationRule,
               data = data.frame(allHistory), method = "LogitBoost", metric = "ROC",
               trControl = logisticControl)
     save(logitBoostFit, file = logisticModelPath)
@@ -135,12 +215,7 @@ if(updateModel){
     load(logisticModelPath)
 }
 
-## logitBoostPredicted = predict(logitBoostFit, testing)
-## confusionMatrix(data = logitBoostPredicted, testing$valid)
-
 ## Model (2): least squares svm radial basis function kernel 
-svmControl = trainControl(method = "cv", number = 5)
-lssvmModelPath = paste0(R_SWS_SHARE_PATH, "/lssvmValidationModel")
 
 ## system.time(
 ##     {
@@ -155,21 +230,15 @@ lssvmModelPath = paste0(R_SWS_SHARE_PATH, "/lssvmValidationModel")
 if(updateModel){
     library(kernlab)
     lssvmRadialFit =
-        lssvm(valid ~ geographicAreaM49 + measuredItemCPC + timePointYears +
-                  flagObservationStatus + flagMethod,
+        lssvm(validationRule,
               data = data.frame(allHistory), kernel = "rbfdot")
     save(logitBoostFit, file = lssvmModelPath)
 } else {
     load(lssvmModelPath)
 }
 
-## lssvmRadialPredicted = predict(lssvmRadialFit, testing)
-## confusionMatrix(data = lssvmRadialPredicted, testing$valid)
-
 
 ## Model (3): Simple random forest
-rfControl = trainControl(method = "cv", number = 5)
-rfModelPath = paste0(R_SWS_SHARE_PATH, "/rfValidationModel")
 
 if(updateModel){
     rfFit =
@@ -177,8 +246,7 @@ if(updateModel){
         ##                       timePointYears, flagObservationStatus,
         ##                       flagMethod)],
         ##       y = training$valid,
-        train(valid ~ geographicAreaM49 + measuredItemCPC + timePointYears +
-                  flagObservationStatus + flagMethod,
+        train(validationRule,
               data = data.frame(allHistory), 
               method = "rf", prox = TRUE, allowParallel = TRUE,
               trControl = rfControl)
@@ -188,18 +256,12 @@ if(updateModel){
 }
     
 
-## rfPredicted = predict(rfFit, testing)
-## confusionMatrix(data = rfPredicted, testing$valid)
-
 
 ## Model (4): flexible discriminant analysis
-fdaModelPath = paste0(R_SWS_SHARE_PATH, "/fdaValidationModel")
-fdaControl = trainControl(method = "cv", number = 5)
 
 if(updateModel){
     fdaFit =
-        train(valid ~ geographicAreaM49 + measuredItemCPC + timePointYears +
-                  flagObservationStatus + flagMethod,
+        train(validationRule, 
               data = data.frame(allHistory),
               method = "fda",  trControl = fdaControl)
     save(fdaFit, file = fdaModelPath)
@@ -207,19 +269,13 @@ if(updateModel){
     load(fdaModelPath)
 }
 
-## fdaPredicted = predict(fdaFit, testing)
-## confusionMatrix(data = fdaPredicted, testing$valid)
-
 
 
 ## Model (5): bagged MARS
-bagEarthModelPath = paste0(R_SWS_SHARE_PATH, "/bagEarthValidationModel")
-bagEarthControl = trainControl(method = "cv", number = 3)
 
 if(updateModel){
     bagEarthFit =
-        train(valid ~ geographicAreaM49 + measuredItemCPC + timePointYears +
-                  flagObservationStatus + flagMethod,
+        train(validationRule,
               data = data.frame(allHistory),
               method = "bagEarth",  trControl = bagEarthControl,
               tuneGrid = data.frame(degree = 1, nprune = 20))
@@ -238,10 +294,14 @@ getValidationData = function(){
     validationData = GetData(swsContext.datasets[[1]])
     validationData = validationData[geographicAreaM49 %in% allCountries, ]
     validationData[, `:=`(c("geographicAreaM49", "measuredItemCPC",
+                            "measuredElement",
                             "timePointYears", "flagMethod",
                             "flagObservationStatus"),
                           list(factor(geographicAreaM49, levels = allCountries), 
-                               factor(measuredItemCPC), as.numeric(timePointYears),
+                               factor(measuredItemCPC),
+                               factor(measuredElement,
+                                      levels = c("5312", "5510", "5412")),
+                               as.numeric(timePointYears),
                                factor(flagMethod),
                                factor(flagObservationStatus)))]
     validationData
@@ -252,19 +312,23 @@ validateData = function(validationData,
     validationModel = list("logitBoostFit", "lssvmRadialFit", "fdaFit",
         "rfFit", "bagEarthFit")){
     validated = copy(validationData)
-    validated[, `:=`(c("logitBoostClass", "lssvmRadialClass", "fdaClass",
-                       "rfClass", "bagEarthClass"),
-                     lapply(validationModel,
-                            FUN = function(x) predict(get(x), newdata = .SD)))]
-    
+
+    predictedClass = sapply(validationModel, FUN = function(x) paste0(x, "Class"))
+
     warningSign = function(x){
         abs(as.numeric(as.character(x)) - 1)
     }
-
+    check <<- copy(validated)
+    validated[, `:=`(c(predictedClass),
+                     lapply(validationModel,
+                            FUN = function(x){
+                                warningSign(predict(get(x, envir = .GlobalEnv),
+                                                    newdata = validated))
+                                }))]
     validated[, `:=`(c("Severity"),
-                     rowSums(sapply(c("logitBoostClass", "lssvmRadialClass",
-                                      "fdaClass", "rfClass", "bagEarthClass"),
-                                FUN = function(x) warningSign(.SD[[x]]))))]
+                     rowSums(validated[, predictedClass, with = FALSE],
+                             na.rm = TRUE))]
+    ## Added description
     validated[Severity == 0, Description := "Value is valid"]
     validated[Severity %in% c(1:3),
               Description :=
@@ -274,22 +338,13 @@ validateData = function(validationData,
               Description :=
                   paste0("Marked value requires revision, with Severity of (",
                          Severity, ")")]    
-    validated[, `:=`(c("logitBoostClass", "lssvmRadialClass", "fdaClass",
-                       "rfClass", "bagEarthClass"), NULL)]
+    validated[, `:=`(c(predictedClass), NULL)]
     validated
 }
 
 
-## validationData = getValidationData()
-## validationData = validationData[flagMethod != "s", ]
-## validationData[, flagMethod := factor(flagMethod)]
-## validatedData =
-##     validateData(validationData,
-##                  validationModel = list("logitBoostFit", "fdaFit",
-##                      "rfFit", "bagEarthFit"))
-
 getValidationData() %>%
-    .[flagMethod != "s", ] %>%
+    .[!flagMethod %in% c("s", "n"), ] %>%
     .[, flagMethod := factor(flagMethod)] %>%
     validateData(.,
                  validationModel = list("logitBoostFit", "fdaFit",
@@ -298,15 +353,7 @@ getValidationData() %>%
                    dataset = "agriculture",
                    validation = .)
 
-## SaveValidation(domain = "agriculture",
-##                dataset = "agriculture",
-##                validation = validatedData[1:5, ])
-
-
-## with(valid, table(valid, validationLevel))
-## valid[, misclassificationRate :=
-##           ifelse(validationLevel == 0 & valid == "0", 1, 0)]
-## table(valid$misclassificationRate)
+## NOTE (Michael): Need to watch out for new levels.
 
 ## Actually, it is actually ok to misclassify valid value. because
 ## they are just value which has not been validated. The important
