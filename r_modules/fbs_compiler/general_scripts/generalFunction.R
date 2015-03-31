e7eb86b11161f86649ae2c6b4190b713f0692f69
mkao006 <mkao006@gmail.com>
2015-03-31 05:21:43 +0200

    added function for adding and sorting flags

--- r_modules/fbs_compiler/general_scripts/generalFunction.R
+++ r_modules/fbs_compiler/general_scripts/generalFunction.R
@@ -217,3 +217,22 @@ saveContingencyCaputTable = function(data){
              normalized = FALSE)
 }
              
+
+addFBSFlags = function(data, valueColumns, valuePrefix,
+    flagObsStatusPrefix, flagMethodPrefix){
+
+    dataCopy = copy(data)
+    flagObsColumns = gsub(valuePrefix, flagObsStatusPrefix, valueColumns)
+    flagMethodColumns = gsub(valuePrefix, flagMethodPrefix, valueColumns)
+    
+    dataCopy[, `:=`(c(flagObsColumns), "E")]
+    dataCopy[, `:=`(c(flagMethodColumns), "e")]
+    
+    
+    keyColumns = colnames(dataCopy)[!colnames(dataCopy) %in%
+        c(valueColumns, flagObsColumns, flagMethodColumns)]
+
+    valueTupleOrder = c(t(cbind(valueColumns, flagObsColumns, flagMethodColumns)))
+    setcolorder(dataCopy, neworder = c(keyColumns, valueTupleOrder))
+    dataCopy
+}
