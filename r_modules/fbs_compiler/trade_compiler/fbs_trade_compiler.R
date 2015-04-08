d0d0b671331d6e79efb9787db43a30aa4d119edc
mkao006 <mkao006@gmail.com>
2015-04-08 17:38:50 +0200

    converted trade from kg to tonnes

--- r_modules/fbs_compiler/trade_compiler/fbs_trade_compiler.R
+++ r_modules/fbs_compiler/trade_compiler/fbs_trade_compiler.R
@@ -33,7 +33,17 @@ getTradeData = function(){
         normalized = FALSE,
         pivoting = tradePivot
     )
-
+    
+    ## NOTE (Michael): The unit for trade is in kg while for other
+    ##                 elements are tonnes, so we divide the trade by
+    ##                 1000 to match the unit.
+    if("Value_measuredElementTrade_5600" %in% colnames(tradeQuery))
+        tradeQuery[, Value_measuredElementTrade_5600 :=
+                       computeRatio(Value_measuredElementTrade_5600, 1000)]
+    
+    if("Value_measuredElementTrade_5900" %in% colnames(tradeQuery))
+    tradeQuery[, Value_measuredElementTrade_5900 :=
+                   computeRatio(Value_measuredElementTrade_5900, 1000)]
 
     ## Convert time to numeric
     tradeQuery[, timePointYears := as.numeric(timePointYears)]
