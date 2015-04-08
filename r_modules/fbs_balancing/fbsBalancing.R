7fe9c460e49f6181e292a72915622d02554a2ff4
mkao006 <mkao006@gmail.com>
2015-04-08 21:52:00 +0200

    added function to get pre-balancing table and per capita capu std from the sws

--- r_modules/fbs_balancing/fbsBalancing.R
+++ r_modules/fbs_balancing/fbsBalancing.R
@@ -22,122 +22,42 @@ if(Sys.getenv("USER") == "mk"){
 }
 
 
-getStandardizedItem =
-    GetCodeList(domain = "fbs",
-                dataset = "fbs_prebalanced",
-                dimension = "measuredItemCPC")
+standardizedItem =
+    GetCodeList(domain = "suafbs",
+                dataset = "fbs_prebalance",
+                dimension = "measuredItemSuaFbs")[grep("^S", code)]
 
-preBalanceTable = read.csv("contigency_table_example.csv")
+
+preBalanceTable = getPreBalancingTable()
 setnames(preBalanceTable,
-         old = c("Value_measuredElementCalorie_5510",
-             "Value_measuredElementCalorie_5600", 
-             "Value_measuredElementCalorie_5900",
-             "Value_measuredElementCalorie_5525", 
-             "Value_measuredElementCalorie_5120", 
-             "Value_measuredElementCalorie_5150", 
-             "Value_measuredElementCalorie_FoodTotal", 
-             "Value_measuredElementCalorie_5520", 
-             "Value_measuredElementCalorie_residual"),
-         new = c("Production", "Imports", "Exports", "Seed", "Loss", "Industrial",
-             "Food", "Feed", "Stock"))
+         old = c("Value_measuredElement_250",
+             "Value_measuredElement_251", 
+             "Value_measuredElement_252",
+             "Value_measuredElement_55252", 
+             "Value_measuredElement_51202", 
+             "Value_measuredElement_51502", 
+             "Value_measuredElement_51422", 
+             "Value_measuredElement_55202", 
+             "Value_measuredElement_50712"),
+         new = c("Production", "Imports", "Exports", "Seed", "Loss",
+             "IndustrialUse", "Food", "Feed", "Stock"))
 
 
-getTradeStandardDeviationCaput = function(){
-    tradeStdCaputKey = DatasetKey(
-        domain = "trade",
-        dataset = "stddev_caloriescap",
-        dimensions = list(
-            Dimension(name = "geographicAreaM49",
-                      keys = selectedCountry),
-            Dimension(name = "measuredItemSuaFbs",
-                      keys = standardizedItem),
-            Dimension(name = "measuredElementTrade",
-                      keys = c("SD5600", "SD5900")),
-            Dimension(name = "timePointYears",
-                      keys = selectedYear)
-        )
-    )
 
-    ## Pivot to vectorize yield computation
-    tradeStdCaputPivot = c(
-        Pivoting(code = "geographicAreaM49", ascending = TRUE),
-        Pivoting(code = "measuredItemSuaFbs", ascending = TRUE),
-        Pivoting(code = "timePointYears", ascending = TRUE),        
-        Pivoting(code = "measuredElementTrade", ascending = TRUE)
-    )
 
-    ## Query the data
-    tradeStdCaputQuery = GetData(
-        key = tradeStdCaputKey,
-        flags = TRUE,
-        normalized = FALSE,
-        pivoting = tradeStdCaputPivot
-    )
-
-    setkeyv(tradeStdCaputQuery, cols = c("geographicAreaM49", "timePointYears"))
-    ## Convert time to numeric
-    tradeStdCaputQuery[, timePointYears := as.numeric(timePointYears)]
-}
-
-getFeedRequirementData = function(){
-    feedRequirementKey = DatasetKey(
-        domain = "feed",
-        dataset = "total_feed",
-        dimensions = list(
-            Dimension(name = "geographicAreaM49",
-                      keys = selectedCountry),
-            Dimension(name = "nutrientType",
-                      keys = "1"),
-            Dimension(name = "estimator",
-                      keys = as.character(2:3)),
-            Dimension(name = "feedBaseUnit",
-                      keys = "2"),
-            Dimension(name = yearVar,
-                      keys = selectedYear)
-        )
-    )
-    
-    ## Pivot to vectorize yield computation
-    feedRequirementPivot = c(
-        Pivoting(code = "geographicAreaM49", ascending = TRUE),
-        Pivoting(code = "nutrientType", ascending = TRUE),
-        Pivoting(code = "feedBaseUnit", ascending = TRUE),
-        Pivoting(code = "timePointYears", ascending = TRUE),
-        Pivoting(code = "estimator", ascending = FALSE)
-    )
-
-    ## Query the data
-    feedRequirementQuery = GetData(
-        key = feedRequirementKey,
-        flags = TRUE,
-        normalized = FALSE,
-        pivoting = feedRequirementPivot
-    )
-
-    setkeyv(feedRequirementQuery, cols = c("geographicAreaM49", "timePointYears"))
-    ## Convert time to numeric
-    feedRequirementQuery[, timePointYears := as.numeric(timePointYears)]
-    feedRequirementQuery[, list(geographicAreaM49, timePointYears,
-                                EDemand_lb = Value_estimator_3,
-                                EDemand_ub = Value_estimator_2)]
-}
-
+feedRequirement = getFeedRequirementData()
 
 
 getStructuralZeroParam = function(){
-    read.csv(file = "structuralZeroParameterCPC.csv")
+    GetTableData(schemaName = "ess", tableName = "fbs_structural_zero")
 }
 
     
-## Function to get the compiled contingency table
-getContingencyTable = function(){
-    ## TODO (Michael): Need to wait for the compiler of other elements
-    ##                 to finish and also get Nick to set up the
-    ##                 table.
+
+oldFormat =
     readFBS(file = "adjustedCommodityContigencyTable.csv",
             file0 = "structuralZeroParameter.csv",
             filef = "adjustedFeedRange.csv")
-}
 
 
 test = getContingencyTable()
