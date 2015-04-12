ac23a74c5528910dc3ced849ab8c60aa6aea6abc
mkao006 <mkao006@gmail.com>
2015-04-12 18:04:11 +0200

    changed the arguement for year

--- r_modules/trade_reliability/tradeReliability/getComtradeMirroredData.R
+++ r_modules/trade_reliability/tradeReliability/getComtradeMirroredData.R
@@ -6,26 +6,23 @@
 ##' partner countries which should be pulled.
 ##' @param item A character vector of all the HS commodity codes which should
 ##' be pulled.
-##' @param dataContext A DatasetKey object (this class is defined in the
-##' faosws package).  This is used to specify which years should be pulled.
 ##' 
 ##' @return A data.table object containing trading data for all input
 ##' reporting countries, partner countries, items, and years.
 ##' 
 
-getComtradeMirroredData = function(reportingCountries, partnerCountries, items,
-                                   dataContext){
+getComtradeMirroredData = function(reportingCountries, partnerCountries, items, years){
         dimensions =
             list(Dimension(name = "reportingCountryM49",
                            keys = as.character(allReportingCountryCode)),
                  Dimension(name = "partnerCountryM49",
                            keys = as.character(allPartnerCountryCode)),
                  Dimension(name = "measuredItemHS",
-                           keys = allItem),
+                           keys = items),
                  Dimension(name = "measuredElementTrade",
                            keys = c("5600", "5900")),
                  Dimension(name = "timePointYears",
-                           keys = dataContext@dimensions$timePointYears@keys))
+                           keys = years))
 
     newKey =
         DatasetKey(domain = "trade",
