ed7b40fc96723cb6fb70aad328a48745fbc0531e
Alexander Matrunich <alexander@matrunich.com>
2015-04-16 09:28:54 +0200

    Function to check existance of SWS environment is created

--- r_modules/trade_prevalidation/R/getTradeElementTable.R
+++ r_modules/trade_prevalidation/R/getTradeElementTable.R
@@ -5,6 +5,8 @@ getTradeElementTable <- function(domain  = "trade",
                                  dataset = "ct_raw_tf",
                                  elementVar = "measuredElementTrade") {
   
+  if(!is.SWSEnvir()) stop("No SWS environment detected.")
+  
   faosws::GetCodeList(domain = domain,
                       dataset =  dataset,
                       dimension = elementVar)
