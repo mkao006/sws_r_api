f24cf4cb6c0455fcfbf51b0635d5d15cf0986391
Alexander Matrunich <alexander@matrunich.com>
2015-04-27 15:08:49 +0200

    Explicit join by code

--- r_modules/trade_prevalidation/R/getHSDesc.R
+++ r_modules/trade_prevalidation/R/getHSDesc.R
@@ -3,7 +3,5 @@ getHSDesc <- function(hs) {
   hs <- as.character(hs)
   
   dplyr::left_join(data.table(code = hs),
-                   getAllItems())
-  
-  
+                   getAllItems(), by = "code")
 }
