5be200e4a6c95fae4d8c569e442b102b53972a2f
Alexander Matrunich <alexander@matrunich.com>
2015-04-28 10:03:52 +0200

    Documentation updated

--- r_modules/trade_prevalidation/R/is.SWSEnvir.R
+++ r_modules/trade_prevalidation/R/is.SWSEnvir.R
@@ -1,7 +1,7 @@
 #' Function to check is it SWS environment
 #' 
 #' Detecting existence of SWS Envir in calling env
-#' by counting swsContext.* vars
+#' by looking for swsContext.datasets
 #' 
 #' @return logical value. TRUE if there 
 
