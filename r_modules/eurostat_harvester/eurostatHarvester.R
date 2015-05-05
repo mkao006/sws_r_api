0e2f4a68abb6e097653822bffa5a9a65eeea2e8b
rockclimber112358 <rockclimber112358@gmail.com>
2015-05-05 16:51:48 +0200

    Final updates to first eurostat module

--- r_modules/eurostat_harvester/eurostatHarvester.R
+++ r_modules/eurostat_harvester/eurostatHarvester.R
@@ -16,55 +16,47 @@ if(!exists("DEBUG_MODE") || DEBUG_MODE == ""){
         ## baseUrl = "https://hqlprswsas1.hq.un.fao.org:8181/sws",
         ## token = "a2dd0e14-1cdc-4486-bc4b-1f65d9ecad01"
         baseUrl = "https://hqlqasws1.hq.un.fao.org:8181/sws",
-        token = "4e57a0e6-39ae-447e-a7f7-f4cf9d5524d0"
+        token = "d8e833c8-a837-4713-b1c9-52582d089e06"
     )
+    files = dir("~/Documents/Github/sws_r_api/r_modules/eurostat_harvester/R",
+                full.names = TRUE)
+    sapply(files, source)
     
 } else {
     cat("Working on SWS...\n")
 }
 
-warning("UPDATE THIS WITH DATABASE TABLES!!!")
-dir = "~/Documents/Github/sws_r_api/r_modules/eurostat_harvester/"
 # dir = "~/GitHub/sws_r_api/r_modules/eurostat_harvester/"
-animalMap = fread(input = paste0(dir, "Mapping_ESTAT-CPC_animals.csv"))
-animalMap[, Item := as.character(Item)]
+animalMap = GetTableData(schemaName = "ess",
+                         tableName = "eurostat_cpc_animals")
+## Filter out any duplicate maps, as they shouldn't be here and will cause
+## problems with removeNon1to1.
+animalMap = unique(animalMap)
 animalMap = removeNon1to1(data = animalMap, key1Colname = "animals",
-                          key2Colname = "Item")
-cropMap = fread(input = paste0(dir, "Mapping_ESTAT-FCL_crops.csv"))
-cropMap[, Item := as.character(Item)] # Are these necessary?
-## Two mappings in one table!  THS_T and THS_H map to different elements
-cropMap = cropMap[strucpro == "AR", ]
-cropMap[, c("strucpro", "Ele", "Factor") := NULL, with = FALSE]
-cropMap = removeNon1to1(data = cropMap, key1Colname = "Item",
-                        key2Colname = "crop_pro")
-meatMap = fread(input = paste0(dir, "Mapping_ESTAT-CPC_meat.csv"))
-meatMap[, Item := as.character(Item)]
-## Two mappings in one table!  THS_T and THS_H map to different elements
-meatMap = meatMap[unit == "THS_T", ]
-meatMap[, c("unit", "Ele", "Factor") := NULL, with = FALSE]
+                          key2Colname = "cpc")
+cropMap = GetTableData(schemaName = "ess",
+                       tableName = "eurostat_cpc_crop")
+cropMap = unique(cropMap)
+cropMap = removeNon1to1(data = cropMap, key1Colname = "cpc",
+                        key2Colname = "crop")
+meatMap = GetTableData(schemaName = "ess",
+                       tableName = "eurostat_cpc_meat")
+meatMap = unique(meatMap)
 meatMap = removeNon1to1(data = meatMap, key1Colname = "meat",
-                        key2Colname = "Item")
+                        key2Colname = "cpc")
 
-## Convert Commodity Codes (crops aren't CPC in SWS, but animals and meat are)
+## Convert Commodity Codes
 commodityCPC = data.table(
-    Item = as.character(swsContext.datasets[[1]]@dimensions$measuredItemCPC@keys)
+    cpc = as.character(swsContext.datasets[[1]]@dimensions$measuredItemCPC@keys)
     )
-fcl2cpc = GetTableData(schemaName = "ess", tableName = "fcl_2_cpc")
-cropMap[, Item := formatC(as.numeric(Item), width = 4, flag = "0",
-                          format = "d")]
-cropMap = data.table(merge.data.frame(cropMap, fcl2cpc,
-                                      by.x = "Item", by.y = "fcl"))
-cropMap[, Item := cpc]
-cropMap[, cpc := NULL]
-
-animalItems = merge(commodityCPC, animalMap, by = "Item")
-animalItems = animalItems[, c("Item", "animals"), with = FALSE]
+animalItems = merge(commodityCPC, animalMap, by = "cpc")
+animalItems = animalItems[, c("cpc", "animals"), with = FALSE]
 setnames(animalItems, c("measuredItemCPC", "EurostatCode"))
-cropItems = merge(commodityCPC, cropMap, by = "Item")
-cropItems = cropItems[, c("Item", "crop_pro"), with = FALSE]
+cropItems = merge(commodityCPC, cropMap, by = "cpc")
+cropItems = cropItems[, c("cpc", "crop"), with = FALSE]
 setnames(cropItems, c("measuredItemCPC", "EurostatCode"))
-meatItems = merge(commodityCPC, meatMap, by = "Item")
-meatItems = meatItems[, c("Item", "meat"), with = FALSE]
+meatItems = merge(commodityCPC, meatMap, by = "cpc")
+meatItems = meatItems[, c("cpc", "meat"), with = FALSE]
 setnames(meatItems, c("measuredItemCPC", "EurostatCode"))
 if(max(nrow(animalItems), nrow(meatItems), nrow(cropItems)) == 0)
     stop("No rows match with eurostat items")
@@ -73,23 +65,16 @@ if(max(nrow(animalItems), nrow(meatItems), nrow(cropItems)) == 0)
 countryMap = GetTableData(schemaName = "ess", tableName = "eurostat_m49")
 m49Areas = swsContext.datasets[[1]]@dimensions$geographicAreaM49@keys
 eurostatAreas = countryMap[m49 %in% m49Areas, eurostat]
-eurostatAreas = eurostatAreas[!eurostatAreas %in%
-                                  c("GB", "RE", "MQ", "GR", "GP", "GF")]
+# eurostatAreas = eurostatAreas[!eurostatAreas %in%
+#                                   c("GB", "RE", "MQ", "GR", "GP", "GF")]
 if(length(eurostatAreas) == 0)
     stop("No rows match with eurostat areas")
 
 ## Convert Element Codes
-## Waiting on table to be loaded:
-## elementMap = GetTableData(schemaName = "ess", tableName = "eurostat_element")
-elementMap = data.table(dataset = c("raw_apro_mt_pann", "raw_apro_mt_pann",
-                                    "raw_apro_cpp_crop", "raw_apro_cpp_crop",
-                                    "raw_apro_mt_lscatl", "raw_apro_mt_lsgoat",
-                                    "raw_apro_mt_lssheep", "raw_apro_mt_lspig"),
-                        strucpro = c(NA, NA, "AR", "PR", NA, NA, NA, NA),
-                        meatitem = c(NA, NA, "SL", "SL", NA, NA, NA, NA),
-                        unit = c("THS_HD", "THS_T", NA, NA, rep("THS_HD", 4)),
-                        element = c(5330, 5315, 5312, 5510, rep(5511, 4)),
-                        factor = 1000)
+elementMap = GetTableData(schemaName = "ess", tableName = "eurostat_element")
+if(any(elementMap$factor != 1000))
+    stop("Current code assumes all factors are 1000, but this seems to not ", 
+         "be the case.  The code needs to be updated to handle other factors.")
 fbsElements = swsContext.datasets[[1]]@dimensions$measuredElement@keys
 eurostatElements = elementMap[element %in% fbsElements, ]
 if(nrow(eurostatElements) == 0)
@@ -115,8 +100,12 @@ if(nrow(animalItems) > 0){
                                        keys = eurostatUnits),
                              Dimension(name = "eurostatRawGeo",
                                        keys = eurostatAreas),
+                             ## HACK!  Eurostat time codes are one year off
+                             ## of the SWS years, but only for animals.  So,
+                             ## we have to adjust here and in the extracted
+                             ## data.
                              Dimension(name = "timePointYears",
-                                       keys = eurostatYears)
+                                       keys = eurostatYears - 1)
                      ))
     keyGoat = keyPig
     keyGoat@dataset = "raw_apro_mt_lsgoat"
@@ -131,6 +120,7 @@ if(nrow(animalItems) > 0){
     #stop("Is this still true?  Something's wrong if it is...")
     #identical(sheepData, cattleData)
     newTempData = rbind(newFAOData, pigData, goatData, sheepData, cattleData)
+    ## HACK!  Convert year values back to original to match SWS
     convertCode(data = newTempData, mappingTable = countryMap,
                 keyData = "eurostatRawGeo", newKeyName = "geographicAreaM49",
                 newKeyMap = "m49", oldKeyMap = "eurostat")
@@ -140,9 +130,9 @@ if(nrow(animalItems) > 0){
                 newKeyMap = "element", oldKeyMap = "unit")
     convertCode(data = newTempData, mappingTable = animalMap,
                 keyData = "eurostatRawAgriprod", newKeyName = "measuredItemCPC",
-                newKeyMap = "Ele", oldKeyMap = "animals")
-    ## Eurostat reporting at end of year, so adjust year down one
-    newTempData[, timePointYears := as.numeric(timePointYears) - 1]
+                newKeyMap = "cpc", oldKeyMap = "animals")
+    ## HACK!  Eurostat reports at end of year, so adjust year up one
+    newTempData[, timePointYears := timePointYears + 1]
     newTempData[, eurostatRawMonth := NULL]
     newFAOData = rbind(newFAOData, newTempData)
 }
@@ -169,7 +159,7 @@ if(nrow(cropItems) > 0){
                 newKeyMap = "element", oldKeyMap = "strucpro")
     convertCode(data = cropData, mappingTable = cropMap,
                 keyData = "eurostatRawCroppro", newKeyName = "measuredItemCPC",
-                newKeyMap = "Item", oldKeyMap = "crop_pro")
+                newKeyMap = "cpc", oldKeyMap = "crop_pro")
     newFAOData = rbind(newFAOData, cropData)    
 }
 if(nrow(meatItems) > 0){
@@ -197,7 +187,7 @@ if(nrow(meatItems) > 0){
                 newKeyMap = "element", oldKeyMap = "unit")
     convertCode(data = meatData, mappingTable = meatMap,
                 keyData = "eurostatRawAgriprod", newKeyName = "measuredItemCPC",
-                newKeyMap = "Item", oldKeyMap = "meat")
+                newKeyMap = "cpc", oldKeyMap = "meat")
     meatData[, eurostatRawMeatItem := NULL]
     newFAOData = rbind(newFAOData, meatData)    
 }
@@ -206,12 +196,14 @@ if(nrow(meatItems) > 0){
 flagMappingTable = GetTableData(schemaName = "ess",
                                 tableName = "eurostat_flag_sws")
 setnames(flagMappingTable, c("eurostat_flag", "status",
-                             "method", "descritpion"),
+                             "method", "description"),
          c("flagRawEurostat", "flagObservationStatus",
            "flagMethod", "Metadata"))
 flagMappingTable[is.na(flagObservationStatus), flagObservationStatus := ""]
 newFAOData = merge(newFAOData, flagMappingTable, by = "flagRawEurostat",
                    all.x = TRUE)
+if(any(is.na(newFAOData$flagObservationStatus)))
+    stop("Invalid Eurostat flag!  Please update the flag conversion table.")
 newFAOData[, flagRawEurostat := NULL]
 
 ## Convert Values (Conversion Factors)
@@ -219,29 +211,40 @@ newFAOData[, Value := Value * 1000]
 
 ## Merge with original data to determine which values to overwrite
 keys = names(swsContext.datasets[[1]]@dimensions)
-oldFAOData = GetData(swsContext.datasets[[1]], metadata = TRUE)
-## Convert keys to character for the join
-for(key in keys){
-    oldFAOData[, c(key) := as.character(get(key))]
-    newFAOData[, c(key) := as.character(get(key))]
+oldFAOData = GetData(swsContext.datasets[[1]])
+if(nrow(oldFAOData) > 0){
+    ## Convert keys to character for the join
+    for(key in keys){
+        oldFAOData[, c(key) := as.character(get(key))]
+        newFAOData[, c(key) := as.character(get(key))]
+    }
+    newFAOData = merge(newFAOData, oldFAOData, by = keys, all.x = TRUE)
+    ## Don't overwrite non-missing (old) values with missing (new) values
+    newFAOData = newFAOData[flagObservationStatus.x != "M" |
+                            flagObservationStatus.y == "M", ]
+    newFAOData[, c("Value.y", "flagObservationStatus.y", "flagMethod.y") :=
+                    list(NULL)]
+    setnames(newFAOData, c("Value.x", "flagObservationStatus.x", "flagMethod.x"),
+             c("Value", "flagObservationStatus", "flagMethod"))
 }
-newFAOData = merge(newFAOData, oldFAOData, by = keys, all.x = TRUE)
-## Don't overwrite non-missing (old) values with missing (new) values
-newFAOData = newFAOData[flagObservationStatus.x != "M" |
-                        flagObservationStatus.y == "M", ]
-newFAOData[, c("Value.y", "flagObservationStatus.y", "flagMethod.y") :=
-                list(NULL)]
-setnames(newFAOData, c("Value.x", "flagObservationStatus.x", "flagMethod.x"),
-         c("Value", "flagObservationStatus", "flagMethod"))
 metadata = newFAOData[, c(keys), with = FALSE]
-metadata[, Metadata := "SOURCE"]
+metadata[, Metadata := "GENERAL"]
 metadata[, Metadata_Language := "en"]
 metadata[, Metadata_Group := 1]
 metadata[, Metadata_Element := "COMMENT"]
 metadata[, Metadata_Value := newFAOData[, Metadata]]
+metadata = metadata[Metadata_Value != "/", ]
+## Also write to metadata to indicate it was pulled from Eurostat
+metadataCopy = copy(metadata)
+metadataCopy[, Metadata := "SOURCE"]
+metadataCopy[, Metadata_Language := "en"]
+metadataCopy[, Metadata_Group := 1]
+metadataCopy[, Metadata_Element := "ORGANIZATION"]
+metadataCopy[, Metadata_Value := "Eurostat"]
+
 SaveData(domain = "agriculture", dataset = "agriculture",
-         data = newFAOData[, c(key, "Value", "flagObservationStatus",
+         data = newFAOData[, c(keys, "Value", "flagObservationStatus",
                                "flagMethod"), with = FALSE],
-         metadata = metadata)
+         metadata = rbind(metadata, metadataCopy))
 
 "Module completed!"
