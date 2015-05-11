fc33b3d9636cdc95c80bb74013a028bcc28b8acd
Alexander Matrunich <alexander@matrunich.com>
2015-05-11 14:01:53 +0200

    Output extended

--- r_modules/trade_prevalidation/R/broom_augment.R
+++ r_modules/trade_prevalidation/R/broom_augment.R
@@ -1,5 +1,11 @@
 # Temporary replacement from broom::augment
 
 broom_augment <- function(model) {
-  cbind(model.frame(model), .std.resid = rstandard(model))
+  sm <- summary(model)
+  cbind(model.frame(model), 
+        .hat = hatvalues(model),
+        .cooksd = cooks.distance(model),
+        .fitted = unname(model$fitted.values),
+        .resid  = unname(model$residuals),
+        .std.resid = rstandard(model))
 }
